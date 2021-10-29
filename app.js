e6f227597c101f7f048ddb65bdcc6e193a66a75e
Dovzhenko-olga <73553889+Dovzhenko-olga@users.noreply.github.com>
2021-10-29 20:24:44 +0300

    email verify

--- app.js
+++ app.js
@@ -17,25 +17,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
-// let avatars = [];
-
-// app.post('api/contacts', upload.single('avatar'), async (req, res) => {
-//   console.log(req.file);
-//   const { path: tempDir, originalname } = req.file;
-//   const uploadDir = path.join(__dirname, 'public\\avatars', originalname);
-//   await fs.rename(tempDir, uploadDir);
-//   const image = path.join('product', originalname);
-//   const newAvatar = { ...req.body, id: 1, image };
-//   avatars.push(newAvatar);
-//   res.status(201).json({
-//     status: 'success',
-//     code: 201,
-//     data: {
-//       result: newAvatar
-//     }
-//   })
-// })
-
 app.use('/api/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
 app.use('/api/users', usersRouter)
